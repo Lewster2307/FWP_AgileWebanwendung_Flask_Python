16e9eb4851581ec9b0d5d3dcfb4791e827c091b2
lewin <lewin.rohling@gmail.com>
2023-04-29 01:19:39 +0200

    Made a lot of progress

--- learning_tool.py
+++ learning_tool.py
@@ -1,8 +1,10 @@
-import flask
-from flask import Flask, render_template, send_from_directory, request
+import random, secrets
+
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database_learning_tool.sqlite"
 
 db = SQLAlchemy(app)
@@ -23,7 +25,7 @@ class Subject(db.Model):
     creator: db.Mapped[str] = db.mapped_column(db.ForeignKey(User.id))
 
     def __repr__(self):
-        return f"ID: {self.id}, Subject: {self.name}, Creator: {self.creator.username}\n"
+        return f"ID: {self.id}, Subject: {self.name}, Creator: {self.creator}\n"
 
 
 class Questions(db.Model):
@@ -34,23 +36,62 @@ class Questions(db.Model):
     creator: db.Mapped[str] = db.mapped_column(db.ForeignKey(User.id))
 
     def __repr__(self):
-        return f"ID: {self.id}, Question: {self.question}, Answer: {self.answer}, Subject: {self.subject.name}, Creator: {self.creator.username}\n"
+        return f"ID: {self.id}, Question: {self.question}, Answer: {self.answer}, Subject: {self.subject}, Creator: {self.creator}\n"
 
 
 @app.get("/")
 def index():
-    all_subjects = db.session.execute(db.select(Subject).filter(Subject.id == 1)).scalars().all()
-    return render_template("index.html", subjects=all_subjects)
+    session["current_user"] = 1
+    session["selected_subject"] = 4
+    session["question"] = ""
+    session["answer"] = ""
+    session["flipped"] = False
+
+    current_user_subjects = db.session.execute(db.select(Subject.name).filter(Subject.creator == session["current_user"])).all()
+    get_random_question()
+
+    return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+
+
+def get_random_question():
+    selected_subject_questions = db.session.execute(db.select(Questions).filter(Questions.creator == session["current_user"]).filter(Questions.subject == session["selected_subject"])).all()
+    selected_question_row = random.choice(selected_subject_questions)
+    session["question"] = selected_question_row._mapping["Questions"].question
+    session['answer'] = selected_question_row._mapping["Questions"].answer
 
 
 @app.route("/clicked")
 def clicked():
+    current_user_subjects = db.session.execute(db.select(Subject.name).filter(Subject.creator == session["current_user"])).all()
+
     link = request.args.get("link")
+
+    if link != "flip":
+        session["flipped"] = False
+
     if link == "new_quiz":
-        print("NEW QUIZ")
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    elif link == "subjects":
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    elif link == "progress":
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    elif link == "login":
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    elif link == "new_subject":
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    elif link == "flip":
+        if not session["flipped"]:
+            session["flipped"] = True
+            return render_template("index.html", subjects=current_user_subjects, card_text=session["answer"], flipped=session["flipped"])
+        else:
+            session["flipped"] = False
+            return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    elif link == "correct_answer":
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"],flipped=session["flipped"])
+    elif link == "wrong_answer":
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"],flipped=session["flipped"])
     else:
-        print("else")
-    return render_template("index.html")
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
 
 
 if __name__ == "__main__":
