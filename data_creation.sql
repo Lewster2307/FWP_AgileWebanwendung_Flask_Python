9e432570c1d24cf115f5f54beb0a372aec1dde5b
lewin <lewin.rohling@gmail.com>
2023-04-29 13:26:36 +0200

    Added ability to change subject

--- data_creation.sql
+++ data_creation.sql
@@ -3,20 +3,20 @@ insert into user(id, username, password) values (2, 'nichtLewin', 'SicheresPassw
 
 insert into subject(id, name, creator) values (1, 'Mathematik 1', 1);
 insert into subject(id, name, creator) values (2, 'Mathematik 2', 1);
-insert into subject(id, name, creator) values (3, 'Programmieren 1', 2);
+insert into subject(id, name, creator) values (3, 'Programmieren 1', 1);
 insert into subject(id, name, creator) values (4, 'Programmieren 2', 1);
-insert into subject(id, name, creator) values (5, 'Datenbanken', 2);
+insert into subject(id, name, creator) values (5, 'Datenbanken', 1);
 
-insert into questions(id, question, answer, subject, creator) values (1, 'Was macht a in der folgen Funktion?\na*sin(b * x + c) + d?', 'Streckung/Stauchung in y-Achse', 1, 2);
-insert into questions(id, question, answer, subject, creator) values (2, 'Wie lautet die Ableitung von ln(x)?', '1/x', 1, 2);
-insert into questions(id, question, answer, subject, creator) values (3, 'Was ist das unbestimmte Integral für cosh(x))?', 'sinh(x)+c', 2, 2);
-insert into questions(id, question, answer, subject, creator) values (4, 'Wie rechnet man Polarkoordinaten in rechtwinklige Kooridnaten um?', 'a=r*cos(phi), b=r*sin(phi)', 2, 2);
-insert into questions(id, question, answer, subject, creator) values (5, 'Kann eine Klasse in Java mehrere Interfaces Implementieren?', 'Ja, nicht aber Mehrfachvererbung', 3, 1);
-insert into questions(id, question, answer, subject, creator) values (6, 'Welche Werte deckt der Datentyp byte ab in Java?', '-128 bis 127?', 3, 1);
-insert into questions(id, question, answer, subject, creator) values (7, 'Was macht Reflection?', 'Reflection erlaubt dem Programm sich selbst zu untersuchen und zu modifizieren', 4, 1);
-insert into questions(id, question, answer, subject, creator) values (8, 'Was ist eine synchronized Methode?', 'Eine Methode in der zu jedem Zeitpunkt nur ein Thread sein darf.', 4, 1);
-insert into questions(id, question, answer, subject, creator) values (9, 'Was ist die DDL', 'Data Definition Language?', 5, 1);
-insert into questions(id, question, answer, subject, creator) values (10, 'Was ist die DML', 'Data Manipulation Language?', 5, 1);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (1, 'Was macht a in der folgen Funktion? a*sin(b * x + c) + d?', 'Streckung/Stauchung in y-Achse', 1, 1, 0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (2, 'Wie lautet die Ableitung von ln(x)?', '1/x', 1, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (3, 'Was ist das unbestimmte Integral für cosh(x))?', 'sinh(x)+c', 2, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (4, 'Wie rechnet man Polarkoordinaten in rechtwinklige Kooridnaten um?', 'a=r*cos(phi), b=r*sin(phi)', 2, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (5, 'Kann eine Klasse in Java mehrere Interfaces Implementieren?', 'Ja, nicht aber Mehrfachvererbung', 3, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (6, 'Welche Werte deckt der Datentyp byte ab in Java?', '-128 bis 127?', 3, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (7, 'Was macht Reflection?', 'Reflection erlaubt dem Programm sich selbst zu untersuchen und zu modifizieren', 4, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (8, 'Was ist eine synchronized Methode?', 'Eine Methode in der zu jedem Zeitpunkt nur ein Thread sein darf.', 4, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (9, 'Was ist die DDL', 'Data Definition Language?', 5, 1 ,0, 0);
+insert into questions(id, question, answer, subject, creator, count_correct, count_wrong) values (10, 'Was ist die DML', 'Data Manipulation Language?', 5, 1 ,0, 0);
 
 delete from user;
 delete from subject;
--- learning_tool.py
+++ learning_tool.py
@@ -16,7 +16,7 @@ class User(db.Model):
     password: db.Mapped[str]
 
     def __repr__(self):
-        return f"ID: {self.id}, Username: {self.username}, Password: {self.password}\n"
+        return f"User ID: {self.id}, Username: {self.username}"
 
 
 class Subject(db.Model):
@@ -25,7 +25,7 @@ class Subject(db.Model):
     creator: db.Mapped[str] = db.mapped_column(db.ForeignKey(User.id))
 
     def __repr__(self):
-        return f"ID: {self.id}, Subject: {self.name}, Creator: {self.creator}\n"
+        return f"Subject ID: {self.id}, Name: {self.name}"
 
 
 class Questions(db.Model):
@@ -34,35 +34,39 @@ class Questions(db.Model):
     answer: db.Mapped[str]
     subject: db.Mapped[str] = db.mapped_column(db.ForeignKey(Subject.id))
     creator: db.Mapped[str] = db.mapped_column(db.ForeignKey(User.id))
+    count_correct: db.Mapped[int]
+    count_wrong: db.Mapped[int]
 
     def __repr__(self):
-        return f"ID: {self.id}, Question: {self.question}, Answer: {self.answer}, Subject: {self.subject}, Creator: {self.creator}\n"
+        return f"Question ID: {self.id}, Subject: {self.subject}"
 
 
 @app.get("/")
 def index():
     session["current_user"] = 1
-    session["selected_subject"] = 4
+    session["selected_subject"] = 1
+    session["question_id"] = 0
     session["question"] = ""
     session["answer"] = ""
     session["flipped"] = False
 
-    current_user_subjects = db.session.execute(db.select(Subject.name).filter(Subject.creator == session["current_user"])).all()
+    current_user_subjects = db.session.execute(db.select(Subject.name, Subject.id).filter(Subject.creator == session["current_user"])).all()
     get_random_question()
 
-    return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+    return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
 
 
 def get_random_question():
     selected_subject_questions = db.session.execute(db.select(Questions).filter(Questions.creator == session["current_user"]).filter(Questions.subject == session["selected_subject"])).all()
     selected_question_row = random.choice(selected_subject_questions)
+    session["question_id"] = selected_question_row._mapping["Questions"].id
     session["question"] = selected_question_row._mapping["Questions"].question
     session['answer'] = selected_question_row._mapping["Questions"].answer
 
 
 @app.route("/clicked")
 def clicked():
-    current_user_subjects = db.session.execute(db.select(Subject.name).filter(Subject.creator == session["current_user"])).all()
+    current_user_subjects = db.session.execute(db.select(Subject.name, Subject.id).filter(Subject.creator == session["current_user"])).all()
 
     link = request.args.get("link")
 
@@ -70,28 +74,43 @@ def clicked():
         session["flipped"] = False
 
     if link == "new_quiz":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "subjects":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "progress":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "login":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "new_subject":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "flip":
         if not session["flipped"]:
             session["flipped"] = True
-            return render_template("index.html", subjects=current_user_subjects, card_text=session["answer"], flipped=session["flipped"])
+            return render_template("index.html", subjects=current_user_subjects, card_text=session["answer"], flipped=session["flipped"], selected_subject=session["selected_subject"])
         else:
             session["flipped"] = False
-            return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+            return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "correct_answer":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"],flipped=session["flipped"])
+        question = db.session.execute(db.select(Questions).filter(Questions.id == session["question_id"])).scalar()
+        question.count_correct += 1
+        db.session.merge(question)
+        db.session.commit()
+        get_random_question()
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     elif link == "wrong_answer":
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"],flipped=session["flipped"])
+        question = db.session.execute(db.select(Questions).filter(Questions.id == session["question_id"])).scalar()
+        question.count_wrong += 1
+        db.session.merge(question)
+        db.session.commit()
+        get_random_question()
+        get_random_question()
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
     else:
-        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"])
+        for subject in current_user_subjects:
+            if link == subject.name:
+                session["selected_subject"] = subject.id
+                get_random_question()
+        return render_template("index.html", subjects=current_user_subjects, card_text=session["question"], flipped=session["flipped"], selected_subject=session["selected_subject"])
 
 
 if __name__ == "__main__":
